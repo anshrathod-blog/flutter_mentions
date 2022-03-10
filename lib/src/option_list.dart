a224a2b158111a6694d75f6b534482e9c8bb0400
Ansh-Rathod <anshrathod29@gmail.com>
2022-03-10 20:40:11 +0530

    added branch

--- lib/src/option_list.dart
+++ lib/src/option_list.dart
@@ -22,33 +22,36 @@ class OptionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return data.isNotEmpty
-        ? Container(
-            decoration:
-                suggestionListDecoration ?? BoxDecoration(color: Colors.white),
-            constraints: BoxConstraints(
-              maxHeight: suggestionListHeight,
-              minHeight: 0,
-            ),
-            child: ListView.builder(
-              itemCount: data.length,
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    onTap(data[index]);
-                  },
-                  child: suggestionBuilder != null
-                      ? suggestionBuilder!(data[index])
-                      : Container(
-                          color: Colors.blue,
-                          padding: EdgeInsets.all(20.0),
-                          child: Text(
-                            data[index]['display'],
-                            style: TextStyle(fontSize: 12),
+        ? ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              decoration: suggestionListDecoration ??
+                  BoxDecoration(color: Colors.white),
+              constraints: BoxConstraints(
+                maxHeight: suggestionListHeight,
+                minHeight: 0,
+              ),
+              child: ListView.builder(
+                itemCount: data.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      onTap(data[index]);
+                    },
+                    child: suggestionBuilder != null
+                        ? suggestionBuilder!(data[index])
+                        : Container(
+                            color: Theme.of(context).cardColor,
+                            padding: EdgeInsets.all(20.0),
+                            child: Text(
+                              data[index]['display'],
+                              style: TextStyle(fontSize: 12),
+                            ),
                           ),
-                        ),
-                );
-              },
+                  );
+                },
+              ),
             ),
           )
         : Container();
